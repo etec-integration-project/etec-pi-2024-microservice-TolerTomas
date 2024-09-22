c714c36cc86001644117329a1ce9a68df6a9ed09
tomastoler <tomastoler@gmail.com>
2024-09-22 17:10:52 -0300

    trying to solve problems with the url of the auth server #3

--- src/index.ts
+++ src/index.ts
@@ -6,40 +6,42 @@ import axios from "axios";
 
 const PORT = process.env.PORT || 8080;
 
-let auth_server_token;
+let auth_server_token: null | string = null;
 
 app.listen(PORT, () => {
 	console.log("[FILE SERVER SERVICE]");
 	console.log("[SERVER] listening on port " + PORT);
 
-	axios
-		.post(
-			"/api/servers/login", // (("http://" + process.env.AUTH_SERVER_ADDRESS) as string) + ":5050/api/servers/login", 
-			{
-				name: process.env.AUTH_SERVER_USERNAME as string,
-				password: process.env.AUTH_SERVER_PASSWORD as string,
-			}
-		)
-		.then((data) => {
-			// not registered
-			if (data.data.error) {
-				axios
-					.post(
-						//(("http://" + process.env.AUTH_SERVER_ADDRESS) as string) + ":5050/api/servers/register",
-						"/api/servers/login",
-						{
-							name: process.env.AUTH_SERVER_USERNAME as string,
-							password: process.env.AUTH_SERVER_PASSWORD as string,
-						}
-					)
-					.then((data) => {
-						auth_server_token = data.data.token;
-						console.log({ auth_server_token });
-						return;
-					});
-			}
-			auth_server_token = data.data.token;
-			console.log({ auth_server_token });
-			return;
-		});
+	while (auth_server_token == null) {
+		axios
+			.post(
+				"/api/servers/login", // (("http://" + process.env.AUTH_SERVER_ADDRESS) as string) + ":5050/api/servers/login", 
+				{
+					name: process.env.AUTH_SERVER_USERNAME as string,
+					password: process.env.AUTH_SERVER_PASSWORD as string,
+				}
+			)
+			.then((data) => {
+				// not registered
+				if (data.data.error) {
+					axios
+						.post(
+							//(("http://" + process.env.AUTH_SERVER_ADDRESS) as string) + ":5050/api/servers/register",
+							"/api/servers/login",
+							{
+								name: process.env.AUTH_SERVER_USERNAME as string,
+								password: process.env.AUTH_SERVER_PASSWORD as string,
+							}
+						)
+						.then((data) => {
+							auth_server_token = data.data.token;
+							console.log({ auth_server_token });
+							return;
+						});
+				}
+				auth_server_token = data.data.token;
+				console.log({ auth_server_token });
+				return;
+			});
+	}
 });
