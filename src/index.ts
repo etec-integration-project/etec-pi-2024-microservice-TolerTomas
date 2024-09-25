ed5c841956af990175f112cfd87cc0ca839c4de0
tomastoler <tomastoler@gmail.com>
2024-09-25 18:26:19 -0300

    auth server url issue #5

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 
 import app from "./server";
+import axios from "axios";
 
 // import axios from "axios";
 
@@ -13,72 +14,25 @@ app.listen(PORT, () => {
 	console.log("[SERVER] listening on port " + PORT);
 
 	while (auth_server_token == null) {
-
-		fetch(`http://${process.env.AUTH_SERVER_ADDRESS as string}:5050/api/servers/login`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				name: process.env.AUTH_SERVER_USERNAME as string,
-				password: process.env.AUTH_SERVER_PASSWORD as string,
+		axios
+			.post(`http://${process.env.AUTH_SERVER_ADDRESS as string}/api/servers/login`, {
+				username: process.env.AUTH_SERVER_USERNAME,
+				password: process.env.AUTH_SERVER_PASSWORD,
 			})
-		})
-			.then(response => response.json())
 			.then(data => {
-				console.log({ data });
 				if (data.data.error) {
-					fetch(`http://${process.env.AUTH_SERVER_ADDRESS as string}:5050/api/servers/register`, {
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						body: JSON.stringify({
-							name: process.env.AUTH_SERVER_USERNAME as string,
-							password: process.env.AUTH_SERVER_PASSWORD as string,
+					axios
+						.post(`http://${process.env.AUTH_SERVER_ADDRESS as string}/api/servers/register`, {
+							username: process.env.AUTH_SERVER_USERNAME,
+							password: process.env.AUTH_SERVER_PASSWORD,
 						})
-					})
-						.then(response => response.json())
 						.then(data => {
-							auth_server_token = data.token;
-							console.log({ auth_server_token });
-							return;
-						});
+							auth_server_token = data.data.token;
+							console.log({ auth_server_token });							
+						})
 				}
-				auth_server_token = data.token;
+				auth_server_token = data.data.token;
 				console.log({ auth_server_token });
-				return;
-			});
-
-		// axios
-		// 	.post(
-		// 		"/api/servers/login", // (("http://" + process.env.AUTH_SERVER_ADDRESS) as string) + ":5050/api/servers/login", 
-		// 		{
-		// 			name: process.env.AUTH_SERVER_USERNAME as string,
-		// 			password: process.env.AUTH_SERVER_PASSWORD as string,
-		// 		},
-		// 	)
-		// 	.then((data) => {
-		// 		// not registered
-		// 		if (data.data.error) {
-		// 			axios
-		// 				.post(
-		// 					//(("http://" + process.env.AUTH_SERVER_ADDRESS) as string) + ":5050/api/servers/register",
-		// 					"/api/servers/login",
-		// 					{
-		// 						name: process.env.AUTH_SERVER_USERNAME as string,
-		// 						password: process.env.AUTH_SERVER_PASSWORD as string,
-		// 					}
-		// 				)
-		// 				.then((data) => {
-		// 					auth_server_token = data.data.token;
-		// 					console.log({ auth_server_token });
-		// 					return;
-		// 				});
-		// 		}
-		// 		auth_server_token = data.data.token;
-		// 		console.log({ auth_server_token });
-		// 		return;
-		// 	});
+			})
 	}
 });
